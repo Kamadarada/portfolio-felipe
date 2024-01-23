f2ec75899fe7b2148b0be9223cae6b4eaea26d44
Kamadarada <felipekamada2@gmail.com>
2024-01-23 18:31:05 -0300

    feat: add CV download and portfolio logo

--- src/componentes_body/body/Body.jsx
+++ src/componentes_body/body/Body.jsx
@@ -3,8 +3,10 @@ import styled from 'styled-components'
 import Body_name from "../body_name/body_name";
 import Button from "../button/Button";
 import arrow_down from '../../images/arrow_down.png'
+import logo from '../../images/logo-img.png'
+import pdfFile from '../../../public/CV-Felipe Kamada.pdf'
 
-const AppBody = styled.body`
+const AppBody = styled.div`
     margin-left:20vh;
     margin-top:15vw;
 `
@@ -12,14 +14,35 @@ const AppBody = styled.body`
 const Image = styled.img`
     width:25px;
 `
+const Logo = styled.img`
+    width: 350px;
+    height: 300px;
+    display:flex;
+    margin-left:90vh;
+    margin-top:11vw;
+`
+
+const AppContainer = styled.body`
+    display:flex;
+`
 
 const Body = () => {
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = pdfFile;
+        link.download = 'CV - Felipe de Castro Kamada';
+        link.click();
+    };
+
     return (
-        <AppBody>
-            <Titulo>Desenvolvedor Web</Titulo>
-            <Body_name>Felipe Kamada</Body_name>
-            <Button>Baixar currículo <Image src={arrow_down} alt="download-image" /></Button>
-        </AppBody>
+        <AppContainer>
+            <AppBody>
+                <Titulo>Desenvolvedor Web</Titulo>
+                <Body_name>Felipe Kamada</Body_name>
+                <Button onClick={handleDownload}>Baixar currículo <Image src={arrow_down} alt="download-image" /></Button>
+            </AppBody>
+            <Logo src={logo} alt="" />
+        </AppContainer>
     )
 }
 
